186c6df7f154f68a7ea865e1f7e82727c377e32f
jhoacar <jhoacar.utn@gmail.com>
2022-04-07 02:53:44 +0200

    added logic with methods post and get

--- utn/jhoan_carrero/lessons/lesson_2/index.js
+++ utn/jhoan_carrero/lessons/lesson_2/index.js
@@ -1,28 +1,38 @@
 const express = require("express");
+const url = require('url');
 const app = express();
 
-// POST -> Trabaja con el body de la request -> Archivos, Texto
-// GET -> Trabaja con los parametros de la URL localhost/productos?id=1&price=243
+// POST -> Trabaja con el body de la request -> Archivos, Texto [req.body]
+// GET -> Trabaja con los parametros de la URL localhost/productos?id=1&price=243 [req.url]
 
-// :algo, :id -> Extraer la ruta req.params
+// :algo, :id -> Extraer la ruta [req.params] 
 
 const callback1 = (request,response,next)=>{
 
-    console.log(request.params);
+    console.log(request.url);
     
     next();
 }
 
 const callback2 = (request,response,next)=>{
-    response.send("Pase por la callback1 y estoy en la callback2")
+    response.sendFile(__dirname+"/login.html");
+};
+
+const callback3 = (request,response,next)=>{
+    response.sendFile(__dirname+"/dashboard.html");
 };
 
 
 const authorization = (request,response,next)=>{
+    request.body; // { answer: 42 }
     
+    console.log(request.body);
+    next();
 };
 
-app.get("/:parametro",callback1,callback2);
+app.get("/",callback1,callback2);
+
+app.post("/",authorization,callback3);
 
 
 app.listen(4000,()=>{
