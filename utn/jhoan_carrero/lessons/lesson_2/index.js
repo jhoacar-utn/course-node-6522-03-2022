f188117c743c1abbd7f1f9c8e528953f2858bc01
jhoacar <jhoacar.utn@gmail.com>
2022-04-07 02:17:21 +0200

    added express route

---
+++ utn/jhoan_carrero/lessons/lesson_2/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+// POST -> Trabaja con el body de la request -> Archivos, Texto
+// GET -> Trabaja con los parametros de la URL localhost/productos?id=1&price=243
+
+// :algo, :id -> Extraer la ruta req.params
+
+const callback1 = (request,response,next)=>{
+
+    console.log(request.params);
+    
+    next();
+}
+
+const callback2 = (request,response,next)=>{
+    response.send("Pase por la callback1 y estoy en la callback2")
+};
+
+
+const authorization = (request,response,next)=>{
+    
+};
+
+app.get("/:parametro",callback1,callback2);
+
+
+app.listen(4000,()=>{
+    console.log("Se encuentra habilitado en el puerto 4000")
+});
