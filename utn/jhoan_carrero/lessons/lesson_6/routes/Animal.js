d764933519c45b2803866f559f2fb5ebe3ee75ca
jhoacar <jhoacar.utn@gmail.com>
2022-04-13 19:34:42 +0200

    added file to route animals

---
+++ utn/jhoan_carrero/lessons/lesson_6/routes/Animal.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const route = express.Router();
+
+const {get_animals} = require('./controller/AnimalController');
+
+
+route.get("/animals",get_animals);
+
+module.exports = route;
