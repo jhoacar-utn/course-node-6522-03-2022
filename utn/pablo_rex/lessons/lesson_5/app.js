f51ec41fa4e21e3d10e4828786a2b04430433332
prex989 <62755771+prex989@users.noreply.github.com>
2022-04-14 13:00:29 -0300

    lesson_5 y 6

--- utn/pablo_rex/lessons/lesson_5/app.js
+++ utn/pablo_rex/lessons/lesson_5/app.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 
-const {get_animals} = require('./controller/AnimalController')
+const {get_animals} = require('./controller/AnimalController');
 
-app.get("/", (req,res)=>{
-    return res.json({message:"Welcome"})
+const app = express();
+
+app.get("/",(req,res)=>{
+    return res.json({message:"welcome"});
 })
 
-app.get("animals", get_animals)
+app.get("/animals",get_animals);
 
 
-
-app.listen(8000, ()=>{
-    console.log("Server is Running on port 8000");
+app.listen(8000,()=>{
+    console.log("Servidor corriendo en el puerto 8000");
 })
