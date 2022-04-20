70cffb76b4ff2605475bd1be2fdf287af7622f4e
jhonnyberdeja <jhonny.berdeja@despegar.com>
2022-04-20 19:37:42 -0300

    configurated connection data base

---
+++ utn/jhonny_berdeja/lessons/lesson_8/controllers/welcomeController.js
@@ -0,0 +1,20 @@
+
+const {getDb} = require("../config/mongo/connection");
+
+const handleRequest = (request,response,next)=>{
+    
+    const dbConnect = getDb();
+    dbConnect
+        .collection("welcome")
+        .find({})
+        .toArray(function (err, result) {
+            if (err) 
+                response.status(400).send("Error fetching welcome collection!");
+            else 
+                response.json(result);
+        })
+    //Constate de NODE => __dirname
+    //response.sendFile(__dirname+"/views/welcome.html");
+}
+
+module.exports.handleWelcome = handleRequest;
