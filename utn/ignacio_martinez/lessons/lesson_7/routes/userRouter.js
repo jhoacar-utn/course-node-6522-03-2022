66555ab268c2b08e9230f5a96cea0a5a0bdbb629
nacho1054 <nacho1054@hotmail.com>
2022-04-20 19:11:21 -0300

    connection whit mongo

---
+++ utn/ignacio_martinez/lessons/lesson_7/routes/userRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const route = express.Router();
+
+const {getAllUsers, postUser} = require("../controllers/userController");
+
+
+route.get("/");
+
+route.post("/");
+
+module.exports = route;
