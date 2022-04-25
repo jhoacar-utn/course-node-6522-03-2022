65fd31f3c8156c2c6b4a21df4496bab18b9cbb72
jhoacar <jhoacar.utn@gmail.com>
2022-04-25 19:55:08 -0300

    added html for register and login

--- utn/lessons/lesson_9/routes/api.js
+++ utn/lessons/lesson_9/routes/api.js
@@ -3,10 +3,14 @@ const express = require("express");
 const route = express.Router();
 
 const {handleWelcome} = require("../controllers/welcomeController");
+
 const route_users = require("./userRouter");
+
+const route_login = require("./loginRouter");
 
 route.get("/",handleWelcome);
 
 route.use("/user",route_users);
+route.use("/login",route_login);
 
 module.exports = route;
