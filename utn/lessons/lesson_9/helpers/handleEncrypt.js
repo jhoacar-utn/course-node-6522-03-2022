d3ef4c502bc8e961417b23cd4a6189d3c2b59b1d
jhoacar <jhoacar.utn@gmail.com>
2022-04-25 20:58:21 -0300

    added validation error

--- utn/lessons/lesson_9/helpers/handleEncrypt.js
+++ utn/lessons/lesson_9/helpers/handleEncrypt.js
@@ -4,14 +4,15 @@ const bcrypt = require("bcrypt");
 
 const getHashedPassword = (plainPassword) =>{
 
-    const saltRounds = process.env.SALT_ROUNDS_BCRYPT || 10;
+    const saltRounds = parseInt(process.env.SALT_ROUNDS_BCRYPT || 10);
 
     
     const encryptPromise = new Promise((resolve,reject)=>{
         
         bcrypt.hash(plainPassword, saltRounds, function(err, hash) {
+
             if( err || !hash)
-                reject("Password doesn't hashed");
+                reject(err);
             else
                 resolve(hash);
         });
