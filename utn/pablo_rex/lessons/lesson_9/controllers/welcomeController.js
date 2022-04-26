7efc3332dc49a2a9db252a08787bb89797bd51a5
prex989 <62755771+prex989@users.noreply.github.com>
2022-04-25 22:20:08 -0300

    lesson_9

---
+++ utn/pablo_rex/lessons/lesson_9/controllers/welcomeController.js
@@ -0,0 +1,38 @@
+const path = require ('path');
+
+const {getDb} = require("../config/mongo/connection")
+
+
+const handleRequest = (req,res,next)=>{
+
+    const dbConnect = getDb();
+    dbConnect
+      .collection("welcome")
+      .find({})
+      .toArray(function (err, result) {
+        if (err) {
+          res.status(400).send("Error fetching welcome collection!");
+       } else {
+          res.json(result);
+        }
+  });
+    
+  //  res.sendFile(__dirname + "/views/index.html");
+}
+
+const handleWelcomeWeb =  (req,res,next)=>{
+  return res.sendFile(path.resolve(__dirname+"/../views/welcome.html"));
+}
+const handleLoginWeb = (request,response,next)=>{
+  return response.sendFile(path.resolve(__dirname+"/../views/login.html"));
+}
+
+const handleRegisterWeb = (request,response,next)=>{
+  return response.sendFile(path.resolve(__dirname+"/../views/register.html"));
+}
+
+
+module.exports.handleWelcome = handleRequest;
+module.exports.handleWelcomeWeb = handleWelcomeWeb;
+module.exports.handleLoginWeb = handleLoginWeb;
+module.exports.handleRegisterWeb = handleRegisterWeb;
