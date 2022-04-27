10b532489da020d694b0b52c366a516a4e51b346
jhoacar <jhoacar.utn@gmail.com>
2022-04-27 20:37:53 -0300

    fixed api route

--- utn/lessons/lesson_10/routes/api.js
+++ utn/lessons/lesson_10/routes/api.js
@@ -2,11 +2,7 @@ const express = require("express");
 
 const route = express.Router();
 
-const {handleWelcome} = require("../controllers/welcomeController");
-
 const route_users = require("./userRouter");
-
-route.get("/",handleWelcome);
 
 route.use("/user",route_users);
 
