f1cb7e20fdb869b257746bd0430f02ec1a7ae2bb
ClaudioMandujano <claudio.mandujano@gmail.com>
2022-05-02 21:42:39 -0300

    Files upload

---
+++ utn/claudio_mandujano/lessons/lesson_11/helpers/handleJWT.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const {secretKey} = require("../config/config");
+
+const getJSONWebToken = (userData)=>{
+
+    return jwt.sign(userData, secretKey);
+
+}
+
+const verifyJSONWebToken = (token) =>{
+
+    return jwt.verify(token, secretKey);
+
+}
+
+module.exports = {
+    getJSONWebToken,
+    verifyJSONWebToken
+}
---
+++ utn/claudio_mandujano/lessons/lesson_11/routes/loginRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const route = express.Router();
+
+const {loginUser,logoutUser} = require("../controllers/userController");
+
+route.post("/",getAllUsers);
+
+route.post("/",postUser);
+
+module.exports = route;
