7c8e804a797de38ae18e91bd6fafed90ff839683
prex989 <62755771+prex989@users.noreply.github.com>
2022-05-02 21:55:27 -0300

    added lesson_11

---
+++ utn/pablo_rex/lessons/lesson_11/routes/userRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const route = express.Router();
+
+const {getAllUsers, postUser, postAvatar} = require("../controllers/userController");
+const uploadMiddleware = require("../middleware/uploadMiddleware");
+
+route.get("/",getAllUsers);
+
+route.post("/",postUser);
+
+route.post("/avatar",uploadMiddleware.single('avatarFile'),postAvatar);
+
+module.exports = route;
+
+
