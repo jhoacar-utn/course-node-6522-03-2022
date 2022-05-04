1ea3b23cb4e60b75d62d06349beb801ed9e90c53
jhonnyberdeja <jhonny.berdeja@despegar.com>
2022-05-04 19:03:31 -0300

    class 11

--- utn/jhonny_berdeja/lessons/lesson_11/controllers/userController.js
+++ utn/jhonny_berdeja/lessons/lesson_11/controllers/userController.js
@@ -1,37 +1,51 @@
 const UserModel = require("../models/mongo/userModel");
 
-const {getHashedPassword} = require("../helpers/handleEncrypt");
+const { getHashedPassword } = require("../helpers/handleEncrypt");
 
-const getAllUsers = (request,response,next)=>{
+const getAllUsers = (request, response, next) => {
 
 };
 
-const postUser = async (request,response,next)=>{
+const postUser = async (request, response, next) => {
 
-    try{
+    try {
 
         const data = request.body;
 
         const plainPassword = data.password;
 
         data.password = await getHashedPassword(plainPassword);
+        data.avatar = "/users/default.png";
 
         const user = new UserModel(data);
-        
+
         await user.save();
 
-        response.json({"user_added": user});
+        response.json({ "user_added": user });
 
 
-    }catch(error){
+    } catch (error) {
         response.status(500);
-        response.json({"server_error":error});
+        response.json({ "server_error": error });
     }
 }
 
-const postAvatar = async (request,response,next)=>{
+const postAvatar = async (request, response, next) => {
 
-    return response.send("Imagen actualizada");
+    try {
+
+        const avatarPath = request.avatarFile;
+        const user = request.user;
+
+        await UserModel.updateOne({ email: user.email }, { avatar: avatarPath });
+
+        return response.redirect("/dashboard");
+    }
+    catch (error) {
+        response.status(500);
+        response.json({ "server_error": error });
+    }
+
 }
 
 
