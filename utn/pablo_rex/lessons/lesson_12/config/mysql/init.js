0d2f3c87dfe8ba1c04f4e2dd3ce22e32e1768cfc
prex989 <62755771+prex989@users.noreply.github.com>
2022-05-04 22:00:44 -0300

    added lesson_12_ with login ok

--- utn/pablo_rex/lessons/lesson_12/config/mysql/init.js
+++ utn/pablo_rex/lessons/lesson_12/config/mysql/init.js
@@ -1,10 +1,13 @@
 
 const sequelize = require("./connection")
+require("../../models");
 
 const initConnection = async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection with mysql has been established successfully.');
+        await sequelize.sync();
+        console.log("Tablas creadas");
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
--- utn/pablo_rex/lessons/lesson_12/server.js
+++ utn/pablo_rex/lessons/lesson_12/server.js
@@ -11,7 +11,7 @@ app.use(express.json()); //This enable the request.body for POST requests
 //app.use(express.json());    // <==== parse request body as JSON;
 app.use(cookieSession({ name: nameCookie, keys: [keyCookie] }))
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 
 const apiRoute = require("./routes/api");
 const webRoute = require("./routes/web");
