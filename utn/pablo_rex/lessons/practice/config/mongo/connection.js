fabcc1935afcf6c37acb5114e1cd2b1f89fc981f
prex989 <62755771+prex989@users.noreply.github.com>
2022-05-04 22:03:18 -0300

    deploy lesson_12

--- utn/pablo_rex/lessons/practice/config/mongo/connection.js
+++ utn/pablo_rex/lessons/practice/config/mongo/connection.js
@@ -5,11 +5,6 @@ const {mongo} = require("../config");
 
 const connectionString = mongo.MONGO_URI;
 
-// main().catch(err => console.log(err));
-
-// async function main() {
-//   await mongoose.connect(connectionString);
-// };
 
 module.exports ={
   connectToServer : async function(){
