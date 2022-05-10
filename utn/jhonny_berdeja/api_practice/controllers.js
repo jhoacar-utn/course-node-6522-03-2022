ed588fea49e0c103b64fc3128bcb6c24394524d4
jhonnyberdeja <jhonny.berdeja@despegar.com>
2022-05-09 21:31:59 -0300

    practie api

---
+++ utn/jhonny_berdeja/api_practice/controllers.js
@@ -0,0 +1,29 @@
+const fetch = require("node-fetch");
+const getAllProducts = async (req, res)=>{
+    let datos = await fetch("https://6275b491bc9e46be1a0e149a.mockapi.io/products")
+    .then(res=>res.json())
+    .catch(err => console.log(err.json))
+
+    res.send(datos)
+}
+
+const postProduct = async (req, res)=>{
+    
+    let dato = req.body;
+    
+    let datoCargado = await fetch("https://6275b491bc9e46be1a0e149a.mockapi.io/products", {
+        method: "POST",
+        body: JSON.stringify(dato),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            }
+    })
+    .then(res=>res.json())
+    .then(res=>JSON.stringify(res))
+    .catch(err=>console.log(err.json()))
+
+    res.send(`Producto cargado: ${datoCargado}`)
+}
+
+module.exports = {getAllProducts, postProduct};
---
+++ utn/jhonny_berdeja/api_practice/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const {getAllProducts, postProduct} = require("./controllers");
+
+router.get("/", getAllProducts);
+
+router.post("/", postProduct);
+
+module.exports=router
