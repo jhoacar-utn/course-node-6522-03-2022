743c29ec9561864c24c7825b6362ac625e7196cc
Guadalupe Migoya <guadalupemigoya@users.noreply.github.com>
2022-05-11 18:37:35 -0300

    added practice api fetch

---
+++ utn/guadalupe_migoya/practices/apiPractice/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000
+const routes =require('./routes')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use("",routes)
+
+app.listen(PORT,()=>{
+    console.log(`server corriendo en el puerto ${PORT}`)
+
+})
---
+++ utn/guadalupe_migoya/practices/apiPractice/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const {getAllProducts,postProduct} = require('./controllers')
+
+router.get("/",getAllProducts);
+
+router.post("/",postProduct);
+
+module.exports= router
