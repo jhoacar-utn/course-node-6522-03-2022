cb42250242f7c8fa35108bdd14793adcc415bed2
juliaribotto <julia.ribotto@mmba.com.ar>
2022-05-12 18:18:03 -0300

    added sync or migration with mysql

--- utn/julia_ribotto/lessons/lesson_12/config/mysql/init.js
+++ utn/julia_ribotto/lessons/lesson_12/config/mysql/init.js
@@ -5,6 +5,9 @@ const initConnection = async () => {
     try {
         await sequelize.authenticate();
         console.log("connection with mysql established");
+        
+        await sequelize.sync();
+        console.log("all models were synchronized successfully");
     } catch {
         console.error("unable to connect to the database:",error);
     }
