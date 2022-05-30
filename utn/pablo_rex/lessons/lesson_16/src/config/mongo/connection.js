cc8e9b35a145c52d872e27681d207dfb24e3f9c9
prex989 <62755771+prex989@users.noreply.github.com>
2022-05-30 16:05:16 -0300

    sintax error fix

--- utn/pablo_rex/lessons/lesson_16/src/config/mongo/connection.js
+++ utn/pablo_rex/lessons/lesson_16/src/config/mongo/connection.js
@@ -1,17 +1,17 @@
-const mogoose =require("mongoose")
-const {uri} = require("../database")
+const mongoose = require("mongoose")
+const { uri } = require("../database")
 
-if (!uri){
+if (!uri) {
   throw 'Se debe especificar la uri de conexion como variable de entorno llamada DB_URI'
 }
 
-const  connection= async ()=>{
+const connection = async () => {
 
   await mongoose.connect(uri);
 }
 
 
 
-module.exports={
+module.exports = {
   connection
 }
