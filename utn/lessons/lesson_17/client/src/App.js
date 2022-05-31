0a6bbef7c7bdd808f1267d944187ca522fe8a4b3
jhoacar <jhoacar.utn@gmail.com>
2022-05-30 22:04:11 -0300

    added component in react

--- utn/lessons/lesson_17/client/src/App.js
+++ utn/lessons/lesson_17/client/src/App.js
@@ -1,8 +1,14 @@
+import MiClaseComponente from "./components/MiClaseComponente";
+import MiComponente from "./components/MiComponente";
 
 function App() {
   return (
     <div className="App">
         Hola Mundo!
+        <hr></hr>
+        <MiComponente></MiComponente>
+        <hr></hr>
+        <MiClaseComponente></MiClaseComponente>
     </div>
   );
 }
