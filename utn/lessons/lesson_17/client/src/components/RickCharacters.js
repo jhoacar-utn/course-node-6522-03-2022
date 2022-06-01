552bb0ffad9af5bc5a8950b323f9ce3a11311d49
jhoacar <jhoacar.utn@gmail.com>
2022-06-01 20:44:48 -0300

    added useEffect

--- utn/lessons/lesson_17/client/src/components/RickCharacters.js
+++ utn/lessons/lesson_17/client/src/components/RickCharacters.js
@@ -1,12 +1,37 @@
-import {getAllCharacters} from '../services/api';
+import { useEffect, useState } from 'react';
+import { getAllCharacters } from '../services/api';
 
 
-function RickCharacter ()
-{
+function RickCharacter() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+
+        getAllCharacters()
+            .then(arrayCharacters => {
+
+                console.log(arrayCharacters);
+                setCharacters(arrayCharacters);
+
+            }).catch(error => {
+                console.log(error);
+            })
+
+    }, []);
 
     return (
         <div>
-            All Characters: 
+            All Characters:
+            <ul>
+                {characters.map((element) => {
+                    return (
+                        <li key={element.name}>
+                            <div>{element.name}</div>
+                            <img src={element.image}></img>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     )
 
