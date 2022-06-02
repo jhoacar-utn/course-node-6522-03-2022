c96bc4b9aef93a188917fe73a2a573bb9f1d7f0f
jhoacar <jhoacar.utn@gmail.com>
2022-06-01 22:02:44 -0300

    added material cards

--- utn/lessons/lesson_17/client/src/components/Characters/index.js
+++ utn/lessons/lesson_17/client/src/components/Characters/index.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import { getAllCharacters } from '../../services/api';
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
 
 function RickCharacter() {
 
@@ -21,19 +25,37 @@ function RickCharacter() {
     }, []);
 
     return (
-        <div>
-            All Characters:
-            <ul>
+        <Card>
+            <Typography sx={{ textAlign: 'center' }} component="h3" variant="h3">
+                All Characters
+            </Typography>
+            <Grid component="ul" sx={{display:"grid", alignItems:"center", justifyContent:"center"}}>
                 {characters.map((element) => {
                     return (
-                        <li key={element.name}>
-                            <div>{element.name}</div>
-                            <img src={element.image}></img>
-                        </li>
+                        <Card
+                            component="li"
+                            sx={{ maxWidth: 300 }}
+                            key={element.name}>
+                            <CardMedia
+                                component="img"
+                                width="100"
+                                image={element.image}
+                                alt={element.name}
+                            >
+                            </CardMedia>
+                            <CardContent>
+                                <Typography
+                                    variant="h5"
+                                    component="h5"
+                                >
+                                    {element.name}
+                                </Typography>
+                            </CardContent>
+                        </Card>
                     );
                 })}
-            </ul>
-        </div>
+            </Grid>
+        </Card>
     )
 
 }
