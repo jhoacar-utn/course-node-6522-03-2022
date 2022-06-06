096f52d0b55171c59b01c639c17b8dfea05fcff9
jhoacar <jhoacar.utn@gmail.com>
2022-06-06 20:35:33 -0300

    added styles in card using material in login

--- utn/lessons/lesson_17/client/src/pages/Login.js
+++ utn/lessons/lesson_17/client/src/pages/Login.js
@@ -1,27 +1,32 @@
-import { Button, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
+import { Button, Card, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
 
 export default function Login() {
     return (
-        <div>
-            <form style={{
+        <form>
+            <Card sx={{
+                minHeight: '500px',
+                minWidth: '500px',
                 display: 'flex',
                 flexDirection: 'column',
-                gap: '50px'
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: '50px',
+                padding: '10px'
             }}>
                 <FormControl >
-                    <InputLabel style={{ color: 'black' }} htmlFor="my-input">Email address</InputLabel>
-                    <Input style={{ color: 'black' }} id="my-input" aria-describedby="my-helper-text" />
-                    <FormHelperText style={{ color: 'black' }} id="my-helper-text">We'll never share your email.</FormHelperText>
+                    <InputLabel htmlFor="my-input">Email address</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text" />
+                    <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl >
-                    <InputLabel style={{ color: 'black' }} htmlFor="my-input">Password</InputLabel>
-                    <Input style={{ color: 'black' }} id="password" aria-describedby="password-helper-text" />
-                    <FormHelperText style={{ color: 'black' }} id="password-helper-text">Please type your password.</FormHelperText>
+                    <InputLabel htmlFor="my-input">Password</InputLabel>
+                    <Input id="password" aria-describedby="password-helper-text" />
+                    <FormHelperText id="password-helper-text">Please type your password.</FormHelperText>
                 </FormControl>
                 <Button type="submit">
                     Iniciar sesion
                 </Button>
-            </form>
-        </div>
+            </Card>
+        </form>
     )
 }
