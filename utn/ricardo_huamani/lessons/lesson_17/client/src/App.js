89096193097ffdcc312234a740c9bc8d049721ea
Ricardo <rhuamani90@gmail.com>
2022-06-08 18:44:03 -0300

    Update lesson_17 at 18:43 8/6/22

--- utn/ricardo_huamani/lessons/lesson_17/client/src/App.js
+++ utn/ricardo_huamani/lessons/lesson_17/client/src/App.js
@@ -1,16 +1,34 @@
-import MiClaseComponente from "./components/MiClaseComponente";
-import MiComponente from "./components/MiComponente";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
-function App() {
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark', // 'dark' or 'light'
+  },
+});
+
+export default function App() {
   return (
-    <div className="App">
-        Hola Mundo!
-        <hr></hr>
-        <MiComponente></MiComponente>
-        <hr></hr>
-        <MiClaseComponente></MiClaseComponente>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Toaster position="bottom-right" reverseOrder={false} />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ utn/ricardo_huamani/lessons/lesson_17/client/src/services/authentication.js
@@ -0,0 +1,17 @@
+
+const API_URL = '/api/v1/auth';
+
+export const handleLogin = async (email, password) => {
+
+    // return 'ok';
+
+    const response = await fetch(API_URL + '/login');
+    const jsonResponse = await response.json();
+
+    if (jsonResponse.error)
+        throw jsonResponse.error;
+
+    return {
+        success: jsonResponse.message
+    }
+}
