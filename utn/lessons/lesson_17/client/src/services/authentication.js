cb874ccddd8692568f9847d449a5a3d3baf9ec23
jhoacar <jhoacar.utn@gmail.com>
2022-06-08 20:03:00 -0300

    added correct logic in fetch for the server

--- utn/lessons/lesson_17/client/src/services/authentication.js
+++ utn/lessons/lesson_17/client/src/services/authentication.js
@@ -4,8 +4,16 @@ const API_URL = process.env.REACT_APP_API_URL || '/api/v1/auth';
 export const handleLogin = async (email, password) => {
 
     // return 'ok';
+    const data = { email, password };
 
-    const response = await fetch(API_URL + '/login');
+    const response = await fetch(API_URL + '/login', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
     const jsonResponse = await response.json();
 
     if (jsonResponse.error)
