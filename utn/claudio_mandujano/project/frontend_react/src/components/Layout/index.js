b874278122e65a1faa5f0a210d3da766d9267315
ClaudioMandujano <claudio.mandujano@gmail.com>
2022-06-12 20:23:33 -0300

    save all files characters

---
+++ utn/claudio_mandujano/project/frontend_react/src/components/Layout/index.js
@@ -0,0 +1,26 @@
+import NavBar from "../Navbar/index";
+import Footer from "../Footer";
+import { Card } from "@mui/material";
+
+
+
+export default function Layout({ children }) {
+
+    return (
+        <div>
+            <NavBar></NavBar>
+                <Card sx={{
+                    minHeight: '85vh',
+                    display: 'flex' ,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color:'black',
+                    backgroundColor: 'dark'//'rgb(12, 176, 241)'
+
+                }}>
+                    {children}
+                </Card>
+            <Footer></Footer>
+        </div>
+    )
+}
---
+++ utn/claudio_mandujano/project/frontend_react/src/components/Login/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import { useState } from "react";
+
+const URL = "http://localhost:5000/login";
+
+const CompLogin = () => {
+
+
+}
+
+export default CompLogin
