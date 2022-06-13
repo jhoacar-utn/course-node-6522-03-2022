c9ebdec6284e17d0d83b3aa3e7b00c042f9f35bd
jhoacar <jhoacar.utn@gmail.com>
2022-06-13 20:33:11 -0300

    added inputs in register form

--- utn/lessons/lesson_17/client/src/pages/Register/index.js
+++ utn/lessons/lesson_17/client/src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import { Button, Card, FormControl, FormHelperText, Input, InputLabel } from "@mui/material";
+import { Button, Card, FormControl, FormHelperText, Input, InputLabel, MenuItem, Select } from "@mui/material";
 import { useContext, useState } from "react"
 import { Navigate } from "react-router-dom";
 import { AuthorizationContext } from "../../context/authorization"
@@ -34,6 +34,28 @@ export default function Register() {
         })
     };
 
+    const handleChangeName = (event) => {
+        setRegisterState({
+            ...registerState,
+            name: event.target.value
+        })
+    };
+
+    const handleChangeAvatar = (event) => {
+        setRegisterState({
+            ...registerState,
+            avatar: event.target.value
+        })
+    };
+
+    const handleChangeImage = (event) => {
+        setRegisterState({
+            ...registerState,
+            image: event.target.value
+        })
+    };
+
+
     return (
         <>
             {isLoggedIn && <Navigate to="/dashboard"></Navigate>}
@@ -48,18 +70,47 @@ export default function Register() {
                     gap: '50px',
                     padding: '10px'
                 }}>
-                    <FormControl >
+                    <FormControl sx={{
+                        width: "50%"
+                    }}>
+                        <InputLabel>User name</InputLabel>
+                        <Input type="text" value={name} onChange={handleChangeName} />
+                    </FormControl>
+                    <FormControl sx={{
+                        width: "50%"
+                    }} >
                         <InputLabel>Email address</InputLabel>
                         <Input type="email" value={email} onChange={handleChangeEmail} />
                         <FormHelperText>We'll never share your email.</FormHelperText>
                     </FormControl>
-                    <FormControl >
+                    <FormControl sx={{
+                        width: "50%"
+                    }}>
                         <InputLabel>Password</InputLabel>
                         <Input type="password" value={password} onChange={handleChangePassword} />
                         <FormHelperText>Please type your password.</FormHelperText>
                     </FormControl>
-                    <Button type="submit">
-                        Iniciar sesion
+                    <FormControl sx={{
+                        width: "50%"
+                    }}>
+                        <Select value={avatar} onChange={handleChangeAvatar}>
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl sx={{
+                        width: "50%"
+                    }}>
+                        {
+                            image.length > 0 &&
+                            <img src={image}></img>
+                        }
+                    </FormControl>
+                    <Button type="submit" sx={{
+                        padding: '20px'
+                    }}>
+                        Registrar
                     </Button>
                 </Card>
             </form>
