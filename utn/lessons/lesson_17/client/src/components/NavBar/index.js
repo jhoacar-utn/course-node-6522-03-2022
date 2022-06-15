9d5df92e3971624f4445c74bd8ddc0051487eb5d
jhoacar <jhoacar.utn@gmail.com>
2022-06-15 20:41:29 -0300

    added redux logic using useContext and useReducer

--- utn/lessons/lesson_17/client/src/components/NavBar/index.js
+++ utn/lessons/lesson_17/client/src/components/NavBar/index.js
@@ -13,20 +13,30 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import { AuthorizationContext } from '../../context/authorization';
 import { saveToken } from '../../services/authentication';
 import { ThemeContext } from '../../context/theme';
+import { StoreContext } from '../../context/store';
+import { CHANGE_LOGGED_IN, CHANGE_DARK_MODE } from '../../reducers/action';
 
 export default function NavBar() {
 
-  const { isLoggedIn, setIsLoggedIn } = useContext(AuthorizationContext)
-
-  const { isDarkMode, setIsDarkMode } = useContext(ThemeContext);
+  const { globalState, setGlobalState } = useContext(StoreContext);
+  const { isLoggedIn, isDarkMode } = globalState;
 
   const handleLogout = () => {
-    setIsLoggedIn(false);
+    // setIsLoggedIn(false);
+    setGlobalState({
+      type: CHANGE_LOGGED_IN,
+      payload: false
+    })
     saveToken("");
   }
 
-  const handleChangeTheme = ()=>{
-    setIsDarkMode(!isDarkMode);
+  const handleChangeTheme = () => {
+
+    //setIsDarkMode(!isDarkMode);
+    setGlobalState({
+      type: CHANGE_DARK_MODE,
+      payload: !globalState.isDarkMode
+    })
   }
 
   return (
