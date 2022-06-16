7974a2e46f81f22d0c7d9a2f1f14da41783debab
jhoacar <jhoacar.utn@gmail.com>
2022-06-15 21:54:56 -0300

    added logic for use database in the backend

--- utn/lessons/lesson_17/server/src/routes/api/auth.js
+++ utn/lessons/lesson_17/server/src/routes/api/auth.js
@@ -1,54 +1,9 @@
 const express = require("express");
+const { handleRegister, handleLogin } = require("../../controllers/auth");
 
 const router = express.Router();
 
-router.post("/login", (req,res)=>{
-
-    const {email, password} = req.body;
-
-    if(!email || !password)
-        return res.json({
-            error: "email and passwords are required"
-        })
-
-    if(email!= 'test@test.com')
-        return res.json({
-            error: "user not registered"
-        })
-
-    if(password!= 'test')
-        return res.json({
-            error: "password incorrect"
-        })
-
-    return res.json({
-        message: "user logged in succesfully",
-        body: {
-            token: "tu token"
-        }
-    })
-    
-});
-
-router.post("/register", (req,res)=>{
-
-    const {name, email, password, avatar,image } = req.body;
-
-    if(!name || !email || !password || !avatar || !image)
-        return res.json({
-            error: "name, email, password, avatar and image are required"
-        });
-
-    return res.json({
-        message: "user registered successfully",
-        body:{
-            name,
-            email,
-            avatar,
-            image
-        }
-    });
-
-});
+router.post("/login", handleLogin);
+router.post("/register", handleRegister);
 
 module.exports = router;
