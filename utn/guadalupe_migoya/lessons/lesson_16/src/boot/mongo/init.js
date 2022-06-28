167b5ea9f4c0dd9a667aad58fe025dd6f2d1526d
Guadalupe Migoya <guadalupemigoya@users.noreply.github.com>
2022-06-28 13:26:10 -0300

    lessons added to my branch

---
+++ utn/guadalupe_migoya/lessons/lesson_16/src/boot/mongo/init.js
@@ -0,0 +1,13 @@
+const { connection } = require("../../config/mongo/connection");
+
+const initialization = async () => {
+    try {
+        await connection();
+        console.log("Conexion establecida con mongo");
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+module.exports = initialization;
